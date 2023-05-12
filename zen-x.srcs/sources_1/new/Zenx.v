bc1a5b72d4dcaf4cf956f736e3de126c390dfb44
calint <calin.tenitchi@gmail.com>
2023-05-12 06:40:03 +0200

    .

--- zen-x.srcs/sources_1/new/Zenx.v
+++ zen-x.srcs/sources_1/new/Zenx.v
@@ -69,7 +69,7 @@ wire cs_ret = is_cs_op && instr_r; // enabled if 'return'
 wire [ROM_ADDR_WIDTH-1:0] cs_pc_out; // 'pc' before the 'call'
 wire cs_zf_out; // zero-flag before the 'call'
 wire cs_nf_out; // negative-flag before the 'call'
-reg cs_en; // used to coordinate push/pop and Zn
+reg cs_en; // used to coordinate call / ret and Zn
 
 // Registers related wiring (part 1)
 wire [REGISTERS_WIDTH-1:0] regs_dat_a; // regs[a]
