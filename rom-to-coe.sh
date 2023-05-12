15186583c4ea10b5d63169073d9b24024068e433
calint <calin.tenitchi@gmail.com>
2023-05-12 11:37:29 +0200

    .

--- rom-to-coe.sh
+++ rom-to-coe.sh
@@ -7,5 +7,5 @@ echo "process '$input_file' to '$output_file'"
 
 echo "memory_initialization_radix=16;" > "$output_file"
 echo -n "memory_initialization_vector=" >> "$output_file"
-grep -v "^#" "$input_file" | tr -s ' ' '\n' | tr -d '\r' | sed '/^\s*$/d' | paste -sd ' ' >> "$output_file"
+grep -v "^#" "$input_file" | tr -s ' ' | tr -d '\r' | sed '/^\s*$/d' | paste -sd ' ' >> "$output_file"
 echo ";" >> "$output_file"
