25ec0334e0bfe0d0fc63dae487a9726ed0df16ba
calint <calin.tenitchi@gmail.com>
2023-05-19 00:38:15 +0200

    .

--- zen-x.srcs/sources_1/new/Sys.v
+++ zen-x.srcs/sources_1/new/Sys.v
@@ -13,10 +13,8 @@ module Sys (
     output wire led0_b
 );
 
-//localparam ROM_FILE = "/home/c/w/zen-x/zen-x.srcs/sim_1/new/rom-tb-zenx.hex";
-//localparam ROM_FILE = "/home/c/w/zen-x/zen-x.srcs/sim_2/new/rom-tb-zenx.hex";
+//localparam ROM_FILE = "/home/c/w/zen-x/zen-x.srcs/sim_1/new/TB_Zenx.hex";
 localparam ROM_FILE = "/home/c/w/zen-x/zen-x.srcs/sim_4/new/TB_ZenxHex.hex";
-//localparam ROM_FILE = "/home/c/w/zen-x/notes/sample.hex";
 localparam CLK_FREQ = 66_000_000;
 localparam BAUD_RATE = 9600;
 // baud rates: 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400
